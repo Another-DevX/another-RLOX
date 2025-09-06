4228f9c258222a24e4592fd60cb3ac525ac2b47c
anotherdev <joseluismanco37@gmail.com>
2025-09-05 21:09:21 -0500

    chore: REPL

--- src/main.rs
+++ src/main.rs
@@ -14,29 +14,16 @@ use scanner::Scanner;
 use token::{Literal, Token};
 
 fn main() {
-    let expression = Expr::Binary {
-        left: Box::new(Expr::Unary {
-            operator: Token::new(token::TokenType::Minus, "-", None, 1),
-            right: Box::new(Expr::Literal(Literal::Number(123.0))),
-        }),
-        operator: Token::new(token::TokenType::Star, "*", None, 1),
-        right: Box::new(Expr::Grouping(Box::new(Expr::Literal(Literal::Number(
-            45.67,
-        ))))),
-    };
-
-    println!("{:?}", AstInterpreter.print(&expression));
-
-    // let args: Vec<_> = std::env::args().skip(1).collect();
-    // let mut lox = Lox::new();
-    // match args.as_slice() {
-    //     [] => run_prompt(&mut lox),
-    //     [path] => run_file(&mut lox, path).unwrap(),
-    //     _ => {
-    //         eprintln!("Usage: jlox [script]");
-    //         std::process::exit(64)
-    //     }
-    // }
+    let args: Vec<_> = std::env::args().skip(1).collect();
+    let mut lox = Lox::new();
+    match args.as_slice() {
+        [] => run_prompt(&mut lox),
+        [path] => run_file(&mut lox, path).unwrap(),
+        _ => {
+            eprintln!("Usage: jlox [script]");
+            std::process::exit(64)
+        }
+    }
 }
 
 fn run_file(lox: &mut Lox, path: &str) -> std::io::Result<()> {
